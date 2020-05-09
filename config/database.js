3f5a1c62101960782a0f28781d45d0bd54fd84e4
rjagait <rjagait@uwo.ca>
2020-05-09 00:30:47 -0400

    Adding database config file for reference

---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+	//update below with: mongodb+srv://username:password@gettingstarted-mk3qu.mongodb.net/listDetails?retryWrites=true&w=majority
+    mongo_url: '',
+    port: 3000,
+    url: 'http://localhost:3000/api',
+	//update below with sendgrid key generated
+    sendgrid_key: ''
+}
